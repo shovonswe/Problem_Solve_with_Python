091111dc455c0416ab1cd0bb37d120e80b772812
Shovon Islam <hasanshovon864@gmail.com>
2025-02-26 02:14:05 +0600

    Update missing number.py

--- pynativee/missing number.py
+++ pynativee/missing number.py
@@ -4,7 +4,8 @@ class  Solution:
             for i in range (len(nums)+1):
                   expected_sum = expected_sum + i
             list_sum = sum(nums)
-            return expected_sum - list_sum      
+            return expected_sum - list_sum 
+           
 nums = [1,4,2,3,9,7,8,5,0]
 Solution = Solution()    
 print(Solution.missingNumber(nums))        
