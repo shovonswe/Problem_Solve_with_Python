a55b93ed0970974c7b6cb995bc25ed9f8f891074
Shovon Islam <hasanshovon864@gmail.com>
2025-04-15 14:23:01 +0600

    Create 1929. Concatenation of Array.py

---
+++ pynativee/1929. Concatenation of Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums):
+        c = nums+nums
+        return c
+Solution = Solution()
+nums = [1,2,3,4,5]
+print(Solution.getConcatenation(nums))    
+            
