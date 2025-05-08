dcd839d53342260399f7016252516656fe683df9
Shovon Islam <hasanshovon864@gmail.com>
2025-05-09 01:01:49 +0600

    Create 238. Product of Array Except Self.py

---
+++ pynativee/238. Product of Array Except Self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
+Solution = Solution()
+nums = [1,2,3,4]
+print(Solution.productExceptSelf(nums))
