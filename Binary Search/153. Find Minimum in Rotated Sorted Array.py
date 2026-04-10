86ff975c5df74429b91cc5483044ffc366c74526
Shovon Islam <163632061+shovonswe@users.noreply.github.com>
2026-04-10 16:24:16 +0600

    Update 153. Find Minimum in Rotated Sorted Array.py

--- Binary Search/153. Find Minimum in Rotated Sorted Array.py
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -1,3 +1,15 @@
-nums = [3,4,5,1,2]
-nums.sort()
-print(nums[0])
+#optimal(log n)
+class Solution:
+    def findMin(self, nums):
+        l,r = 0,len(nums)-1
+        while l<r:
+            mid = (l+r) // 2
+            if  nums [mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[r]
+                    
+nums = [4,5,6,7,0,1,2]
+tSolution = Solution()
+print(tSolution.findMin(nums))    
